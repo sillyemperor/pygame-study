2ddc587315c7b775aa3f9bddd6a1d1fe2ade1c1b
WJ <sillyemperor@qq.com>
2019-10-25 21:53:23 +0800

    完成碰撞球

--- rebound-ball-walls.py
+++ rebound-ball-walls.py
@@ -4,6 +4,24 @@
 """
 import sys, pygame
 import os.path
+import random
+import time
+import math
+#pygame.font.init()
+#myfont = pygame.font.SysFont('Comic Sans MS', 30)
+
+def hit_A(x, y):
+    return y <= 10
+
+def hit_B(x, y):
+    return x >= 590
+
+def hit_C(x, y):
+    return y >= 470
+
+def hit_D(x, y):
+    return x <= 10
+
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -13,14 +31,41 @@ pygame.init()
 
 screen = pygame.display.set_mode((600, 480))
 
-ball_center = (30, 30)
-ball_color = [0, 255, 0]
 
+ball_color = [255, 0, 0]
+
+x, y = random.randint(10, 590), random.randint(10, 390)
+speed = random.randint(100, 300)
+u = random.random()*math.pi
+f = 20
+t = time.time()
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
     screen.fill((255, 255, 255))
+    t1 = time.time()
+    dt = t1-t
+    d = speed*dt
+    dx, dy = d*math.cos(u), d*math.sin(u)
+    x += dx
+    y += dy
+    if hit_B(x, y):
+        u = math.pi - u
+    elif hit_C(x,y):
+        u = 2*math.pi - u
+    elif hit_D(x,y):
+        u = 3*math.pi - u
+    elif hit_A(x, y):
+        u = 2*math.pi - u
+    speed -= f*dt
+    if speed < 0:
+        speed = 0
     # 在前进方向上移动一步
-    pygame.draw.circle(screen, ball_color, ball_center, 10)
+    pygame.draw.circle(screen, ball_color, (int(x), int(y)), 10)
+
+    #textsurface = myfont.render(f'{dx} {dy}', False, (0, 0, 0))
+    #screen.blit(textsurface,(x,y))
+    
     pygame.display.flip()
+    t = t1
